1b947df08d620476731f5a224f55868b44debceb
Andrew Hett <andrewmhett@gmail.com>
2021-01-02 16:20:36 -0800

    Add loading spinner to index page

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import flask
 import os
+import hashlib
+from server import Server
+
+servers={}
 
 '''
 Heroku assigns this app a port to use
@@ -11,6 +15,69 @@ app=flask.Flask(__name__)
 
 @app.route("/")
 def landing_page():
+    if flask.request.args["key"] not in servers.keys():
+        servers[flask.request.args["key"]]=Server()
     return flask.render_template('loading_page.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return flask.render_template('dashboard.html')
+
+@app.route("/heartbeat")
+def heartbeat():
+    id_hash=flask.request.args["key"]
+    global servers
+    server=servers[id_hash]
+    queue_str=""
+    for action in server.action_queue:
+        queue_str.append(action+",\n")
+    action_queue=[]
+    return queue_str
+
+@app.route("/currently_playing_information")
+def currently_playing():
+    id_hash=flask.request.args["key"]
+    global servers
+    server=servers[id_hash]
+    if flask.request.method=="GET":
+        ret_json={
+            "current_name":server.current_song,
+            "seconds_elapsed:":server.seconds_elapsed,
+            "total_length":server.length
+        }
+        return str(ret_json)
+    else:
+        server.current_song=flask.request.json["current_name"]
+        server.seconds_elapsed=flask.request.json["seconds_elapsed"]
+        server.length=flask.request.json["total_length"]
+        return 0
+
+@app.route("/song_queue")
+def song_queue():
+    id_hash=flask.request.args["key"]
+    global servers
+    server=servers[id_hash]
+    if flask.request.method=="GET":
+        queue_str=""
+        for song in server.song_queue:
+            queue_str.append(song+",\n")
+        return queue_str
+    else:
+        self.song_queue=flask.request.data.split(",\n")
+        return 0
+
+@app.route("/playlist_names")
+def playlists():
+    id_hash=flask.request.args["key"]
+    global servers
+    server=servers[id_hash]
+    if flask.request.method=="GET":
+        playlist_str=""
+        for playlist in server.playlists:
+            playlist_str.append(playlist+",\n")
+        return playlist_str
+    else:
+        server.playlists=flask.data.split(",\n")
+        return 0
 
 app.run(host="0.0.0.0", threaded=True, port=heroku_port)
---
+++ server.py
@@ -0,0 +1,7 @@
+class Server:
+    def __init__(self):
+        self.song_queue=[]
+        self.playlists=[]
+        self.current_song=""
+        self.seconds_elapsed=0
+        self.length=0
