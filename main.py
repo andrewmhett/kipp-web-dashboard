bf6f5f2b137db1528b51f4ba1047d1cf225f88b6
Andrew Hett <andrewmhett@gmail.com>
2021-01-02 17:36:58 -0800

    Add RSA authentication to POST requests

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from server import Server
 
 servers={}
 
+def authenticate(signature,hash):
+    from pubkey import e, n
+    derived_hash=pow(int(signature),e,n)
+    if derived_hash==int(hash):
+        return 0
+    return 1
+
 '''
 Heroku assigns this app a port to use
 with the PORT environment variable
@@ -33,50 +40,71 @@ def heartbeat():
     action_queue=[]
     return queue_str
 
-@app.route("/currently_playing_information")
+@app.route("/currently_playing_information",methods = ['GET', 'POST'])
 def currently_playing():
     id_hash=flask.request.args["id_hash"]
     global servers
-    server=servers[id_hash]
-    if flask.request.method=="GET":
-        ret_json={
-            "current_name":server.current_song,
-            "seconds_elapsed:":server.seconds_elapsed,
-            "total_length":server.length
-        }
-        return str(ret_json)
-    else:
-        server.current_song=flask.request.json["current_name"]
-        server.seconds_elapsed=flask.request.json["seconds_elapsed"]
-        server.length=flask.request.json["total_length"]
-        return 0
+    try:
+        server=servers[id_hash]
+        if flask.request.method=="GET":
+            ret_json={
+                "current_name":server.current_song,
+                "seconds_elapsed:":server.seconds_elapsed,
+                "total_length":server.length
+            }
+            return str(ret_json)
+        else:
+            if "signature" in flask.request.args.keys() and "hash" in flask.request.args.keys():
+                auth_code=authenticate(flask.request.args["signature"],flask.request.args["hash"])
+                if auth_code==0:
+                    server.current_song=flask.request.json["current_name"]
+                    server.seconds_elapsed=flask.request.json["seconds_elapsed"]
+                    server.length=flask.request.json["total_length"]
+                    return "Authentication Succeeded",200
+            return "Authentication Failed",401
+    except KeyError:
+        return "Server dashboard not initialized",404
 
-@app.route("/song_queue")
+@app.route("/song_queue",methods = ['GET', 'POST'])
 def song_queue():
     id_hash=flask.request.args["id_hash"]
     global servers
-    server=servers[id_hash]
-    if flask.request.method=="GET":
-        queue_str=""
-        for song in server.song_queue:
-            queue_str.append(song+",\n")
-        return queue_str
-    else:
-        self.song_queue=flask.request.data.split(",\n")
-        return 0
+    try:
+        server=servers[id_hash]
+        if flask.request.method=="GET":
+            queue_str=""
+            for song in server.song_queue:
+                queue_str.append(song+",\n")
+            return queue_str
+        else:
+            if "signature" in flask.request.args.keys() and "hash" in flask.request.args.keys():
+                auth_code=authenticate(flask.request.args["signature"],flask.request.args["hash"])
+                if auth_code==0:
+                    self.song_queue=str(flask.request.data).split(",\n")
+                    return "Authentication Succeeded",200
+            return "Authentication Failed",401
+    except KeyError:
+        return "Server dashboard not initialized",404
 
-@app.route("/playlist_names")
+@app.route("/playlist_names",methods = ['GET', 'POST'])
 def playlists():
     id_hash=flask.request.args["id_hash"]
     global servers
-    server=servers[id_hash]
-    if flask.request.method=="GET":
-        playlist_str=""
-        for playlist in server.playlists:
-            playlist_str.append(playlist+",\n")
-        return playlist_str
-    else:
-        server.playlists=flask.data.split(",\n")
-        return 0
+    try:
+        server=servers[id_hash]
+        if flask.request.method=="GET":
+            playlist_str=""
+            for playlist in server.playlists:
+                playlist_str.append(playlist+",\n")
+            return playlist_str
+        else:
+            if "signature" in flask.request.args.keys() and "hash" in flask.request.args.keys():
+                auth_code=authenticate(flask.request.args["signature"],flask.request.args["hash"])
+                if auth_code==0:
+                    server.playlists=str(flask.request.data).split(",\n")
+                    return "Authentication Succeeded",200
+            return "Authentication Failed",401
+    except KeyError:
+        return "Server dashboard not initialized",404
 
 app.run(host="0.0.0.0", threaded=True, port=heroku_port)
